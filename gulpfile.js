b9b561922eeb0b6284a420184c74f7dc1c453f06
Henning Christiansen <henning.christiansen@webstep.no>
2014-03-05 11:24:06 +0100

    adding banner to minified css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@ var clean = require('gulp-clean');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
+var header = require('gulp-header');
+var moment = require('moment');
+
+var banner = '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+	'<%= today("YYYY-MM-DD") %>\n' +
+	'* Copyright (c) <%= today("YYYY") %> ' +
+	'<%= pkg.author.name %> */\n';
 
 gulp.task('clean', function() {
 	gulp.src('app/dist')
@@ -15,11 +22,19 @@ gulp.task('less', function() {
 		.pipe(gulp.dest('app/dist'));
 });
 
-gulp.task('cssmin', function(){
+gulp.task('cssmin', function() {
 	gulp.src(['app/dist/*.css', '!app/dist/*.min.css'])
-	.pipe(cssmin())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('app/dist'));
+		.pipe(cssmin())
+		.pipe(header(banner, {
+			pkg: require('./package.json'),
+			today: function(pattern) {
+				return moment().format(pattern)
+			}
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('app/dist'));
 })
 
 gulp.task('default', ['clean']);
