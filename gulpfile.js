692bd1c7eaee60981a60dacafbbcb8626bd3e6a7
Henning Christiansen <henning.christiansen@webstep.no>
2014-03-05 12:10:20 +0100

    adding uglify task

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var cssmin = require('gulp-cssmin');
 var header = require('gulp-header');
 var moment = require('moment');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 var banner = '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
 	'<%= today("YYYY-MM-DD") %>\n' +
@@ -41,6 +42,15 @@ gulp.task('cssmin', function() {
 gulp.task('concat', function() {
 	gulp.src('app/scripts/**/*.js')
 		.pipe(concat('app.js'))
+		.pipe(gulp.dest('app/dist'));
+});
+
+gulp.task('uglify', function() {
+	gulp.src('app/dist/app.js')
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
 		.pipe(gulp.dest('app/dist'));
 });
 
