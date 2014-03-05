0d6ee49d0711d5e04750c489b45bfb5e268f4049
Henning Christiansen <henning.christiansen@webstep.no>
2014-03-05 10:57:45 +0100

    adding less task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var less = require('gulp-less');
+
+gulp.task('clean', function() {
+	gulp.src('app/dist')
+		.pipe(clean());
+});
+
+gulp.task('less', function() {
+	gulp.src('app/styles/less/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('app/dist'));
+});
+
+gulp.task('default', ['clean']);
