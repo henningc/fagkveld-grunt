46f2c6f87152c01c05176710007d8e0cc34bbb94
Henning Christiansen <henning.christiansen@webstep.no>
2014-03-05 11:12:33 +0100

    adding cssmin task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var less = require('gulp-less');
+var rename = require('gulp-rename');
+var cssmin = require('gulp-cssmin');
 
 gulp.task('clean', function() {
 	gulp.src('app/dist')
@@ -12,5 +14,12 @@ gulp.task('less', function() {
 		.pipe(less())
 		.pipe(gulp.dest('app/dist'));
 });
+
+gulp.task('cssmin', function(){
+	gulp.src(['app/dist/*.css', '!app/dist/*.min.css'])
+	.pipe(cssmin())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('app/dist'));
+})
 
 gulp.task('default', ['clean']);
